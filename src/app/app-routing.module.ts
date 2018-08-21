23b5690bf6b8c83751727eb81a08680801a1abae
JuCarvalho <jucarvalho@outlook.com.br>
2018-08-21 16:18:03 +0000

    alteração no routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AuthGuard } from './shared';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MithtruePageModule } from './mithtrue-page/mithtrue-page.module';
 import { SmilePageModule } from './smile-page/smile-page.module';
 import { AboutPageModule } from './about-page/about-page.module';
 import { ContactPageModule } from './contact-page/contact-page.module';
 import { NotFoundModule } from './not-found/not-found.module';
 
+export function loadDashboardModule () { return DashboardModule }
+export function loadMithtruePageModule () { return MithtruePageModule }
+export function loadSmilePageModule () { return SmilePageModule }
+export function loadAboutPageModule () { return AboutPageModule }
+export function loadContactPageModule () { return ContactPageModule }
+export function loadNotFoundModule () { return NotFoundModule }
+
 const routes: Routes = [
-    { path: '', loadChildren: () => DashboardModule },
-    { path: 'mith-true', loadChildren: () => MithtruePageModule },
-    { path: 'smile', loadChildren: () => SmilePageModule },
-    { path: 'about', loadChildren: () => AboutPageModule },
-    { path: 'contact', loadChildren: () => ContactPageModule },
-    { path: 'not-found', loadChildren: () => NotFoundModule },
+    { path: '', loadChildren: loadDashboardModule },
+    { path: 'mith-true', loadChildren: loadMithtruePageModule },
+    { path: 'smile', loadChildren: loadSmilePageModule },
+    { path: 'about', loadChildren: loadAboutPageModule },
+    { path: 'contact', loadChildren: loadContactPageModule },
+    { path: 'not-found', loadChildren: loadNotFoundModule },
     { path: '**', redirectTo: 'not-found' }
 ];
+
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
